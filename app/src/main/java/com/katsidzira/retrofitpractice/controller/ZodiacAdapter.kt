b677f5e37e5bc060111890fe5731c1a282a9179f
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MBP-2.home>
2020-03-31 22:19:30 -0400

    done

--- app/src/main/java/com/katsidzira/retrofitpractice/controller/ZodiacAdapter.kt
+++ app/src/main/java/com/katsidzira/retrofitpractice/controller/ZodiacAdapter.kt
@@ -1,4 +1,35 @@
 package com.katsidzira.retrofitpractice.controller
 
-class ZodiacAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.katsidzira.news_app.model.Zodiac
+import com.katsidzira.retrofitpractice.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_single_zodiac.view.*
+import kotlinx.android.synthetic.main.zodiac_list_view.view.*
+
+class ZodiacAdapter(private val zodiacList: List<Zodiac>): RecyclerView.Adapter<ZodiacViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ZodiacViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.zodiac_list_view, parent, false)
+        return ZodiacViewHolder(view)
+    }
+
+    override fun getItemCount() = zodiacList.size
+
+    override fun onBindViewHolder(holder: ZodiacViewHolder, position: Int) {
+        holder.onBind(zodiacList[position])
+    }
+}
+
+class ZodiacViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    fun onBind(zodiac: Zodiac) {
+        itemView.zodiac_name_text.text = zodiac.name
+        itemView.zodiac_date_text.text = zodiac.number
+        Picasso.get().load(zodiac.image).into(itemView.zodiac_image)
+    }
+
 }
--- app/src/main/java/com/katsidzira/retrofitpractice/ui/MainActivity.kt
+++ app/src/main/java/com/katsidzira/retrofitpractice/ui/MainActivity.kt
@@ -3,11 +3,16 @@ package com.katsidzira.retrofitpractice.ui
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.d
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.katsidzira.news_app.model.ZodiacList
 import com.katsidzira.news_app.network.RetrofitClient
 import com.katsidzira.news_app.network.ZodiacService
 import com.katsidzira.retrofitpractice.R
+import com.katsidzira.retrofitpractice.controller.ZodiacAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,16 +27,16 @@ class MainActivity : AppCompatActivity() {
 
         val retrofit = RetrofitClient().getInstance()
 
-        recyclerview.adapter
-
         val zodiacService = retrofit.create(ZodiacService::class.java)
         zodiacService.getZodiacDetails().enqueue(object: Callback<ZodiacList> {
             override fun onResponse(call: Call<ZodiacList>, response: Response<ZodiacList>) {
-                d(TAG, "zodiac: ${response.body()?.zodiacList?.get(0)?.name}")
+                val zodiacList = response.body()?.zodiacList
+                recyclerview.adapter = ZodiacAdapter(zodiacList!!)
+                recyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
             }
 
             override fun onFailure(call: Call<ZodiacList>, t: Throwable) {
-                d(TAG, "failure: ${t.message}")
+                sequenceOf(Toast.makeText(applicationContext, "Unable to get data", Toast.LENGTH_SHORT).show())
             }
 
         })
